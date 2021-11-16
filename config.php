22718ce36e834587359105d90c4eeaa679afe298
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2021-11-16 13:41:07 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect($_ENV["DB_SERVER"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+$link = mysqli_connect($_SERVER["DB_SERVER"], $_SERVER["DB_USERNAME"], $_SERVER["DB_PASSWORD"], $_SERVER["DB_NAME"]);
  
 // Check connection
 if($link === false){
