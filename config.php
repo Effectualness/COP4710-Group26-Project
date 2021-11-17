1b2c9cffdc7b436b7de1ebab0a27974ededfad3f
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2021-11-17 13:16:29 -0500

    working config file

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(getenv('DB_SERVER'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
- 
+//$link = mysqli_connect(getenv('DB_SERVER'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
+echo "h4i";
+echo getenv('DB_SERVER');
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
