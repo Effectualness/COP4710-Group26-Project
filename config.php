852856721b2777dcce6e9271d712bd8b38281357
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2021-11-18 13:54:21 -0500

    local env file functionality

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
+//env parser
+require_once "EnvParser.php";
+use DevCoder\DotEnv;
+(new DotEnv(__DIR__ . '/.env'))->load();
+
 /* Attempt to connect to MySQL database */
-//$link = mysqli_connect(getenv('DB_SERVER'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
-echo "h4i";
-echo getenv('DB_SERVER');
+$link = mysqli_connect(getenv('DB_SERVER'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
