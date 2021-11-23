2fda9417764621ef81bd06992736bfce71cebadc
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2021-11-23 09:44:56 -0500

    Added skeleton pages

---
+++ src/pages/adminManager.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>todo</title>
+</head>
+<body>
+    <h1>todo</h1>
+</body>
--- src/pages/welcome.php
+++ src/pages/welcome.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <body>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="changePassword.php" class="btn btn-warning">Reset Your Password</a>
         <a href="../componenets/logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
 </body>
