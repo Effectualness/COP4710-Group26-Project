62d0ed07df1405627ed48e2e05a038981dc39f77
Enrique Rodriguez <soraestafar@gmail.com>
2021-12-07 15:20:48 -0500

    Update sendEmailAPI.php

--- php/COP4710-Group26-Project-old/src/components/sendEmailAPI.php
+++ php/COP4710-Group26-Project-old/src/components/sendEmailAPI.php
@@ -39,7 +39,7 @@ if (mail($to, $subject, $message, $headers)) {
                 <input style = "height:20px;width:300px;font-size:12pt; type="text" id="emailEntry1" placeholder="Subject"/>
                 <input style="height:200px;width:300px;font-size:12pt rows="2" cols="30" placeholder="Body"></textarea>
                 <input type="date" id="scheduleDate">
-				<button style = "height:40px;width:300px;font-size:12pt; id="emailButton2" type="button" onClick={sendReminderEmail.php}>Send Reminder</button>
+				<button style = "height:40px;width:300px;font-size:12pt; id="emailButton2" type="button" onClick={sendReminderEmailAPI.php}>Send Reminder</button>
             </div>
             
             <h1>Schedule Email Globally</h1>
@@ -47,14 +47,14 @@ if (mail($to, $subject, $message, $headers)) {
                 <input style = "height:20px;width:300px;font-size:12pt; type="text" id="emailEntry1" placeholder="Subject"/>
                 <input style="height:200px;width:300px;font-size:12pt rows="2" cols="30" placeholder="Body"></textarea>
                 <input type="date" id="scheduleDate">
-				<button style = "height:40px;width:300px;font-size:12pt; id="emailButton2" type="button" onClick={sendReminderEmail.php}>Send Reminder</button>
+				<button style = "height:40px;width:300px;font-size:12pt; id="emailButton2" type="button" onClick={sendReminderEmailAPI.php}>Send Reminder</button>
             </div>
             
             <h1>Request Book Information</h1>
             <div class="wrapper"> 
             <input style = "height:20px;width:300px;font-size:12pt"; type="text" id="emailEntry2" placeholder="email@domain.com"/>
             
-                <button style = "height:40px;width:300px;font-size:12pt"; id="emailButton" type="button" onClick={sendEmail.php} >Send Request</button>
+                <button style = "height:40px;width:300px;font-size:12pt"; id="emailButton" type="button" onClick={sendEmailAPI.php} >Send Request</button>
             </div>
             
             <button style = "height:40px;width:300px;font-size:12pt"; type="button" onClick={returnToDashboard}>Return to Dashboard</button>
