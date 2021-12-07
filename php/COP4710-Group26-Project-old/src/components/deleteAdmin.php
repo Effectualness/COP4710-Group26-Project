f61a55ffd6687d1e5590595dc265b29d4b1940ba
JosephStewart0101 <88674584+JosephStewart0101@users.noreply.github.com>
2021-12-06 23:14:28 -0500

    Update deleteAdmin.php

--- php/COP4710-Group26-Project-old/src/components/deleteAdmin.php
+++ php/COP4710-Group26-Project-old/src/components/deleteAdmin.php
@@ -1,1 +1,24 @@
-todo
+<?php
+header("Access-Control-Allow-Headers: Content-type");
+header("Access-Control-Allow-Origin: *");
+
+include "database.php";
+
+$inputData = json_decode(file_get_contents('php://input'), true);
+
+$username = $inputData["username"];
+
+$connection = mysqli_connect($host, $username, $password, $dbname);
+if($connection->connect_error)
+{
+  die("Connection terminated." . $conn->connect_error);
+}
+
+$deleteSql = "DELETE FROM employee WHERE username=" . $username . ";";
+
+# Not verifying deletion, there isn't a case where that would be
+# feature-breakingfor the user.
+
+# Does this need the super admin's password to perform action?
+
+$connection->close();
