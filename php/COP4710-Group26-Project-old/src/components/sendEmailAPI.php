e8a0471a21100da05af275fa867473f612476813
Enrique Rodriguez <soraestafar@gmail.com>
2021-12-07 15:14:13 -0500

    Update sendEmailAPI.php

--- php/COP4710-Group26-Project-old/src/components/sendEmailAPI.php
+++ php/COP4710-Group26-Project-old/src/components/sendEmailAPI.php
@@ -19,3 +19,37 @@ if (mail($to, $subject, $message, $headers)) {
     die("Error: Email was not sent successfully");
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div className="schedulePage">
+            <h1>Schedule Email</h1>
+            <div class="wrapper">
+                <input style = "height:20px;width:300px;font-size:12pt; type="text" id="emailEntry2" placeholder="email@domain.com"/>
+                
+                <input style = "height:20px;width:300px;font-size:12pt; type="text" id="emailEntry1" placeholder="Subject"/>
+                <input style="height:200px;width:300px;font-size:12pt rows="2" cols="30" placeholder="Body"></textarea>
+                <input type="date" id="scheduleDate">
+				<button style = "height:40px;width:300px;font-size:12pt; id="emailButton2" type="button" onClick={scheduleInviteEmail}>Send Reminder</button>
+            </div>
+            
+            <h3>Request Book Information</h3>
+            <div class="wrapper"> 
+            <input style = "height:20px;width:300px;font-size:12pt; type="text" id="emailEntry2" placeholder="email@domain.com"/>
+            
+                <button style = "height:40px;width:300px;font-size:12pt; id="emailButton" type="button" onClick={sendEmail} >Send Request</button><br />
+            </div>
+            
+            <button type="button" onClick={doGoBack}>Return to Dashboard</button><br />
+        </div>
+</body>
+</html>
