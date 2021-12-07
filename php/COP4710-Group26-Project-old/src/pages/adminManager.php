a40b001d6ba488e86235dd96583742bb442058d5
Enrique Rodriguez <soraestafar@gmail.com>
2021-12-07 15:58:02 -0500

    Update adminManager.php

--- php/COP4710-Group26-Project-old/src/pages/adminManager.php
+++ php/COP4710-Group26-Project-old/src/pages/adminManager.php
@@ -2,6 +2,35 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+<div className="creationDeletionPage">
+  <h3>Create/Delete Admin Accounts</h3>
+      <input style = "height:30px;width:300px;font-size:12pt"; type="text" id="username" placeholder="username"/><br/>
+      <input style = "height:30px;width:300px;font-size:12pt"; type="text" id="password" placeholder="password"/><br/>
+      <button type="button" onClick={createAdmin}>Create</button>
+      <button type="button" onClick={deleteAdmin}>Delete</button><br/><br/>
+      </div>
+  	<h3>Change current account password</h3>
+  	<p>Current username: </p>
+  	<input style = "height:30px;width:300px;font-size:12pt"; type="text" id="newPassword" placeholder="new password"/><br/>
+  	<button type="button" onClick={doChangePassword}>Change Password</button>
+    <br/><br/>
+	<button style = "height:40px;width:300px;font-size:12pt"; type="button" onClick={adminHome}>Return to Dashboard</button>
+  </div>
+</body>
+</html>
+<!--
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>Admin Manager</title>
@@ -25,4 +54,4 @@
             </a>
         </div>
     </body>
-</html>
+</html>-->
