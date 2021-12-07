24ebb193d89b77eec1209eb37d95780ca3bb15cd
Enrique Rodriguez <soraestafar@gmail.com>
2021-12-07 15:29:48 -0500

    Update sendEmailAPI.php

--- php/COP4710-Group26-Project-old/src/components/sendEmailAPI.php
+++ php/COP4710-Group26-Project-old/src/components/sendEmailAPI.php
@@ -7,13 +7,14 @@ $inputData = json_decode(file_get_contents('php://input'), true);
 $recipient = $inputData["recipients"];
 $subject = $inputData["subject"];
 $body = $inputData["body"];
+$scheduleDate = $inputData["scheduleDate"];
 
 $headers = "From: group26cop4710bookorders@gmail.com". "\r\n" . "Reply-To: group26cop4710bookorders@gmail.com" . "\r\n";
 		   
 $headers  = "MIME-Version: 1.0" . "\r\n";
 $headers = "Content-type: text/html; charset=iso-8859-1" . "\r\n";
 
-if (mail($to, $subject, $message, $headers)) {
+if (mail($recipient, $subject, $body, $scheduleData, $headers)) {
     echo("Email was sent successfully.");
 } else {
     die("Error: Email was not sent successfully");
