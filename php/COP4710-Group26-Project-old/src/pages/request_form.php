5411e34a945fe8eb10026fbe58a93f3614dc1b82
Aquila0210 <75435146+Aquila0210@users.noreply.github.com>
2021-12-07 15:57:17 -0500

    Update request_form.php

--- php/COP4710-Group26-Project-old/src/pages/request_form.php
+++ php/COP4710-Group26-Project-old/src/pages/request_form.php
@@ -10,24 +10,24 @@ if (isset($_POST["name"])) {
  
 <!-- (B) ORDER FORM -->
 <form id="orderform" method="post" target="_self">
-  <label for="Book Title">Name:</label>
-  <input type="text" title="title" required value="John Doe"/> 
+  <label for="Book Title">Book Title:</label>
+  <input type="text" title="title" required value="Moby Dick"/> 
  
 <form id="orderform" method="post" target="_self">
-  <label for="Authors Names">Name:</label>
+  <label for="Authors Names">Authors Names:</label>
   <input type="text" name="name" required value="John Doe"/>
 
 <form id="orderform" method="post" target="_self">
-  <label for="Edition">Name:</label>
-  <input type="text" edition="edition" required value="John Doe"/>
+  <label for="Edition">Edition:</label>
+  <input type="text" edition="edition" required value="1st"/>
  
 <form id="orderform" method="post" target="_self">
-  <label for="Publisher">Name:</label>
-  <input type="text" publisher="publisher" required value="John Doe"/>  
+  <label for="Publisher">Publisher:</label>
+  <input type="text" publisher="publisher" required value="Salvatore"/>  
 
 <form id="orderform" method="post" target="_self">
-  <label for="ISBN">Name:</label>
-  <input type="text" isbn="isbn" required value="John Doe"/> 
+  <label for="ISBN">ISBN:</label>
+  <input type="text" isbn="isbn" required value="12345"/> 
 
   <label for="qty">Quantity Needed:</label>
   <input type="number" name="qty" required value="1"/> 
