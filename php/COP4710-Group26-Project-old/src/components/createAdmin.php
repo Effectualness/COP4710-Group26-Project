f25a9f1f60bfd52c779a0e6ef492451aaa0c89b2
Enrique Rodriguez <soraestafar@gmail.com>
2021-12-06 22:14:47 -0500

    Update createAdmin.php

--- php/COP4710-Group26-Project-old/src/components/createAdmin.php
+++ php/COP4710-Group26-Project-old/src/components/createAdmin.php
@@ -1,1 +1,29 @@
-todo
+
+<?php
+header("Access-Control-Allow-Headers: Content-type");
+header("Access-Control-Allow-Origin: *");
+
+include "database.php";
+
+$inputData = json_decode(file_get_contents('php://input'), true);
+
+$username = $inputData["username"];
+$password = $inputData["password"];
+
+$connection = mysqli_connect($host, $username, $password, $dbname);
+if ($connection->connect_error) {
+    die("Connection terminated." . $conn->connect_error);
+}
+
+$checkSql = "SELECT * FROM employee WHERE username=" . $username . " AND password=" . $password . ";";
+$insertSql = "INSERT INTO Employee (username, password, isAdmin) VALUES ('$username', '$password', 1)";
+
+$checkResult = mysqli_query($conn, $checkSql);
+
+if ($checkResult->num_rows > 0) {
+    echo('{"status":"AlreadyExists"}');
+} else if (mysqli_query($conn, $insertSql)) {
+    echo('{"status":"success"}');
+}
+
+$connection->close();
